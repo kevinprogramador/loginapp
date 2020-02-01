708c7e7dec597976f3a5fb2d28542411ba95c9ee
kevinprogramador <kev.araneda@alumnos.duoc.cl>
2020-02-01 15:44:54 -0300

    Creacion de componentes

---
+++ src/app/componentes/registe-page/registe-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registe-page',
+  templateUrl: './registe-page.component.html',
+  styleUrls: ['./registe-page.component.scss']
+})
+export class RegistePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
